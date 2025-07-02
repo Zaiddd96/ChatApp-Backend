356af6262097849585f1b73aad55c87aa83c61a3
zaiddd96 <shaikhdiazdz96@gmail.com>
2025-07-02 17:22:14 +0530

    db update

--- database.py
+++ database.py
@@ -2,7 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:diaz@localhost:5432/student_db"
+
+import os
+
+# Try to read from Railway's environment variable
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Optional: fallback for local development
+if not DATABASE_URL:
+    DATABASE_URL = "postgresql://postgres:diaz@localhost:5432/student_db"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
